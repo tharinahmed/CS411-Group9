e3aef1c491a25e6d0fdc96a55ae7223152b0f203
hayamaj <hayamaj@bu.edu>
2023-03-31 09:54:23 -0400

    first push

---
+++ barbuddy/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="text-5xl font-bold">BarBuddy</h1>
+            <p class="py-6">The easiest way to discover drinks.</p>
+            <button class="btn btn-primary">Let's Explore!</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
