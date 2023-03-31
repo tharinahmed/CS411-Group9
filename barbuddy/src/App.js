b3b7fa1813e2c2bf4b746e030eaa336c2e0f2690
hayamaj <hayamaj@bu.edu>
2023-03-31 10:08:01 -0400

    added components folder

--- barbuddy/src/App.js
+++ barbuddy/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
+import React from "react";
+import HomePage from './components/Homepage';
+
 
 function App() {
   return (
-    <div className="App">
-      <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">BarBuddy</h1>
-            <p class="py-6">The easiest way to discover drinks.</p>
-            <button class="btn btn-primary">Let's Explore!</button>
-          </div>
-        </div>
-      </div>
-    </div>
+  <HomePage />
   );
 }
 
--- barbuddy/tailwind.config.js
+++ barbuddy/tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: 
+    ['node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
   theme: {
     extend: {
 
