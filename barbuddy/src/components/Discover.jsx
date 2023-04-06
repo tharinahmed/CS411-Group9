d39ae6aeec99222c063cc696c0f91a841db06fb9
Naomi Downing <downing3@bu.edu>
2023-04-06 15:28:45 -0400

    added routing

---
+++ barbuddy/src/components/Discover.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Discover(){
+    return (
+        <p>hi! discover drinks here.</p>
+    );
+}
+
+export default Discover;
--- barbuddy/src/components/Homepage.jsx
+++ barbuddy/src/components/Homepage.jsx
@@ -1,12 +1,13 @@
+import React from "react";
+import Cards from "./Cards";
+
 function HomePage() {
     return (
         <div className="App">
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen">
           <div class="hero-content text-center">
             <div class="max-w-md">
-              <h1 class="text-5xl font-bold">BarBuddy</h1>
-              <p class="py-6">The easiest way to discover drinks.</p>
-              <button class="btn btn-primary">Let's Explore!</button>
+              <Cards />
             </div>
           </div>
         </div>
